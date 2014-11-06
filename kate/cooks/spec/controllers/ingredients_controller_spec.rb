5fcb29db36494866f21a8db7e8b52f221ea94653
Kate Fulton <katesuzannefulton@gmail.com>
2014-11-05 22:14:38 -0800

    More spec setup

---
+++ kate/cooks/spec/controllers/ingredients_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails-helper'
+
+describe IngredientsController do
+
+  it "creates ingredient with name and description" do
+    kale = Ingredient.create!(name: "Kale", description: "I'm green and leafy.")
+
+    expect(kale.name).to eq("Kale")
+    expect(kale.description).to eq("I'm green and leafy.")
+  end
+end

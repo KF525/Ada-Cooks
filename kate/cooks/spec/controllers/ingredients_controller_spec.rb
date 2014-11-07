256fdf24b1106e76eb9b2e0f239c65fbf42aa241
Kate Fulton <katesuzannefulton@gmail.com>
2014-11-06 19:08:23 -0800

    Added tests to ingredients controller

--- kate/cooks/spec/controllers/ingredients_controller_spec.rb
+++ kate/cooks/spec/controllers/ingredients_controller_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 describe IngredientsController do
+  before { @ingredient = Ingredient.new }
+  subject { @ingredient }
 
   it "creates ingredient with name and description" do
     kale = Ingredient.create!(name: "Kale", description: "I'm green and leafy.")
@@ -8,5 +10,16 @@ describe IngredientsController do
     expect(kale.name).to eq("Kale")
     expect(kale.description).to eq("I'm green and leafy.")
   end
-  
+
+  context "when name empty" do
+    it { should_not be_valid }
+    specify { @ingredient.save.should == false }
+  end
+
+  context "when name not empty" do
+    before { @ingredient.name = "kale" }
+
+    it { should be_valid }
+    specify {@ingredient.save.should == true }
+  end
 end
